56e259db657540ee6b4e9651287bc839e356b1a0
trevormil <trevormil@comcast.net>
2022-04-13 19:05:31 -0400

    update private api url

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,8 @@ export const NODE_URL = 'https://bit-badges.herokuapp.com';
 
 export const WEBSITE_HOSTNAME = 'localhost:3001';
 
-export const PRIVATE_API_URL = 'https://bitbadges-private-api.herokuapp.com';
+// export const PRIVATE_API_URL = 'https://bitbadges-private-api.herokuapp.com';
+export const PRIVATE_API_URL = 'https://api.circlegame.io';
 // export const PRIVATE_API_URL = 'http://localhost:3000';
 
 export const MAX_DATE_TIMESTAMP = 8640000000000000;
